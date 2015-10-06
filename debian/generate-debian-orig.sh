b25b98363954f768703d1ce52234f54a793c71ee
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2015-10-06 11:50:49 +0900

    git-debimport openjdk-7_7u79-2.5.6-1~deb8u1.debian.tar.xz

---
+++ debian/generate-debian-orig.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+tarballs="corba.tar.bz2 hotspot.tar.bz2 jaxp.tar.bz2 jaxws.tar.bz2 jdk.tar.bz2 langtools.tar.bz2 openjdk.tar.bz2"
+# AArch64 hotspot
+aarch64_hsname=hotspot-aarch64
+tarballs="$tarballs $aarch64_hsname.tar.bz2"
+tarballs="$tarballs icedtea-sound.tar.gz"
+jamvmtb=jamvm-2.0.0.tar.gz
+cacaotb=cacao-e215e36be9fc.tar.gz
+tarballdir=7u79-2
+version=7u79-2.5.6
+base=openjdk-7
+pkgdir=$base-$version
+origtar=${base}_${version}.orig.tar.gz
+
+icedtea_checkout=icedtea-2.5.6
+debian_checkout=openjdk7
+
+if [ -d $pkgdir ]; then
+    echo directory $pkgdir already exists
+    exit 1
+fi
+
+if [ -d $pkgdir.orig ]; then
+    echo directory $pkgdir.orig already exists
+    exit 1
+fi
+
+if [ -f $origtar ]; then
+    echo "Using existing $origtar"
+    tar xf $origtar
+    if [ -d $pkgdir.orig ]; then
+       mv $pkgdir.orig $pkgdir
+    fi
+    tar -c -f - -C $icedtea_checkout . | tar -x -f - -C $pkgdir
+    rm -rf $pkgdir/.hg
+else
+    echo "Creating new $pkgdir.orig/"
+    rm -rf $pkgdir.orig
+    mkdir -p $pkgdir.orig
+    case "$base" in
+      openjdk*)
+        for i in $tarballs; do
+            cp -p $tarballdir/$i $pkgdir.orig/
+        done
+	if [ -n "$cacaotb" ]; then
+          cp -p $tarballdir/$cacaotb $pkgdir.orig/
+	fi
+        cp -p $tarballdir/$jamvmtb $pkgdir.orig/
+      ;;
+    esac
+    tar -c -f - -C $icedtea_checkout . | tar -x -f - -C $pkgdir.orig
+    (
+      cd $pkgdir.orig
+      sh autogen.sh
+      rm -rf autom4te.cache
+    )
+    cp -a $pkgdir.orig $pkgdir.new
+    rm -rf $pkgdir.orig/.hg
+    mv $pkgdir.orig $pkgdir
+    tar cfz $origtar $pkgdir
+    rm -rf $pkgdir
+    mv $pkgdir.new $pkgdir
+fi
+
+echo "Build debian diff in $pkgdir/"
+cp -a $debian_checkout $pkgdir/debian
+(
+  cd $pkgdir
+  #bash debian/update-shasum.sh
+  #bash debian/update-hgrev.sh
+  ls
+  sh autogen.sh
+  rm -rf autom4te.cache
+)
