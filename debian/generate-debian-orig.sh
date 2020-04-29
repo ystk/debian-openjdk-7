ee971e7ce983d0f73a0a66fac69b6d04f2905ec3
Skerlet Development Team <skerlet@swc.toshiba.co.jp>
2020-04-30 01:32:53 +0900

    git-debimport openjdk-7_7u261-2.6.22-1~deb8u1.debian.tar.xz

--- debian/generate-debian-orig.sh
+++ debian/generate-debian-orig.sh
@@ -7,16 +7,16 @@ tarballs="$tarballs icedtea-sound-1.0.1.tar.xz"
 jamvmtb=jamvm-2.0.0.tar.gz
 
 # tarballs location
-tarballdir=7u241
+tarballdir=7u261
 
 # icedtea upstream location (as extracted from icedtea's tarball)
-icedtea_checkout=icedtea-2.6.20
+icedtea_checkout=icedtea-2.6.22
 
 # openjdk's debian location (usually fetched from bzr or the latest openjdk)
 debian_checkout=openjdk-7
 
 base=openjdk-7
-version=7u241-2.6.20
+version=7u261-2.6.22
 
 # output directory
 pkgdir=$base-$version
